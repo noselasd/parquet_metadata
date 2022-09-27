5b848491b4718c857031a9d26f7f84ed525f58da
Nils O. Selåsdal <nos@utel.tech>
2022-09-28 00:15:00 +0200

    Add --schma flag to dump schema only

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,30 @@ use std::fs::File;
 #[clap(version)]
 #[clap(about = "Prints out parquet metadata for each file")]
 struct Args {
+    #[clap(short, long)]
+    #[clap(help = "Print schema only")]
+    schema: bool,
+
     #[clap(value_parser, required(true))]
     #[clap(help = "parquet file(s)")]
     files: Vec<String>,
 }
 
-fn print_metedata(filename: &str) -> Result<(), std::io::Error> {
+fn print_metedata(filename: &str, schema_only: bool) -> Result<(), std::io::Error> {
     let reader_options = ReadOptionsBuilder::new().with_page_index().build();
     let file = File::open(filename)?;
 
     let reader = SerializedFileReader::new_with_options(file, reader_options).unwrap();
     let metadata = reader.metadata();
 
-    println!("{} Meta Data:", filename);
-    printer::print_parquet_metadata(&mut std::io::stdout(), metadata);
+    let output = &mut std::io::stdout();
+    if schema_only {
+        println!("Schema for {}", filename);
+        printer::print_schema(output, metadata.file_metadata().schema())
+    } else {
+        println!("Metadata for  {}", filename);
+        printer::print_parquet_metadata(output, metadata);
+    }
 
     Ok(())
 }
@@ -30,7 +40,7 @@ fn print_metedata(filename: &str) -> Result<(), std::io::Error> {
 fn main() {
     let flags = Args::parse();
     for arg in &flags.files {
-        if let Err(why) = print_metedata(arg) {
+        if let Err(why) = print_metedata(arg, flags.schema) {
             println!("Can't open {} : {}", arg, why);
         }
     }
