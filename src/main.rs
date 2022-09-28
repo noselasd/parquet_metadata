eea0523a9c92a8aeeb6657d1d7a48c2d4a7a8325
Nils O. Selåsdal <nos@utel.tech>
2022-09-28 12:13:35 +0200

    Add options to print more/less details

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,28 @@ use parquet::file::serialized_reader::SerializedFileReader;
 use parquet::schema::printer;
 use std::fs::File;
 
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum InfoType {
+    All,
+    Metadata,
+    Schema,
+}
+
 #[derive(Parser)]
 #[clap(version)]
 #[clap(about = "Prints out parquet metadata for each file")]
 struct Args {
-    #[clap(short, long)]
-    #[clap(help = "Print schema only")]
-    schema: bool,
+    #[clap(long)]
+    #[clap(help = "What to print")]
+    #[clap(value_enum, default_value_t=InfoType::All)]
+    info: InfoType,
 
     #[clap(value_parser, required(true))]
     #[clap(help = "parquet file(s)")]
     files: Vec<String>,
 }
 
-fn print_metedata(filename: &str, schema_only: bool) -> Result<(), std::io::Error> {
+fn print_metedata(filename: &str, info_type: &InfoType) -> Result<(), std::io::Error> {
     let reader_options = ReadOptionsBuilder::new().with_page_index().build();
     let file = File::open(filename)?;
 
@@ -26,21 +34,28 @@ fn print_metedata(filename: &str, schema_only: bool) -> Result<(), std::io::Erro
     let metadata = reader.metadata();
 
     let output = &mut std::io::stdout();
-    if schema_only {
-        println!("Schema for {}", filename);
-        printer::print_schema(output, metadata.file_metadata().schema())
-    } else {
-        println!("Metadata for  {}", filename);
-        printer::print_parquet_metadata(output, metadata);
+    match info_type {
+        InfoType::Schema => {
+            println!("Schema for {}", filename);
+            printer::print_schema(output, metadata.file_metadata().schema());
+        }
+        InfoType::Metadata => {
+            println!("File metadata for {}", filename);
+            printer::print_file_metadata(output, metadata.file_metadata());
+        }
+        InfoType::All => {
+            println!("Metadata for  {}", filename);
+            printer::print_parquet_metadata(output, metadata);
+        }
     }
 
     Ok(())
 }
 
 fn main() {
-    let flags = Args::parse();
-    for arg in &flags.files {
-        if let Err(why) = print_metedata(arg, flags.schema) {
+    let args = Args::parse();
+    for arg in &args.files {
+        if let Err(why) = print_metedata(arg, &args.info) {
             println!("Can't open {} : {}", arg, why);
         }
     }
